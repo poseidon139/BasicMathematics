d5d0ac6bd819b761f34efc04b650e433ec002a30
Surya <surya.vardhan@siemens.com>
2024-01-24 00:52:17 +0530

    added more solutions

---
+++ Josephus.java
@@ -0,0 +1,8 @@
+class Solution{
+    public static void Josephus_count(int n, int k){
+        System.out.println("jfkjk"+ n + k);
+    }
+    public static void main(String args[]){
+        Josephus_count(10,10 );
+    }
+}
